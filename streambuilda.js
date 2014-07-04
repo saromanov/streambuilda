fd02f7102a4f14ede296d47750c2947e4a064db8
Sergey Romanov <xxsmotur@gmail.com>
2014-07-04 21:35:03 +0600

    Some additions

--- streambuilda.js
+++ streambuilda.js
@@ -30,20 +30,20 @@ function GraphQueue(){
 
 }
 
+
 //Чтение файла
 function readFile(name){
-	if(name == 'string'){
+	if(typeof name == 'string'){
 		return files.readFileSync(name, {encoding: 'utf-8'});
 	}
 	else
 	{
 		var result = [];
-		for(var n in names){
-			result.push(files.readFileSync(name, {encoding: 'utf-8'}));
+		for(var n in name){
+			result.push(files.readFileSync(name[n], {encoding: 'utf-8'}));
 		}
 		return result;
 	}
-	//console.log('RRR: ' + content);
 }
 
 function logMessage(message){
@@ -62,33 +62,82 @@ function readFileAsync(name){
 	});
 }
 
+function loadModules(modules){
+	var result = [];
+	for(var m in modules){
+		try
+		{
+			var data = require(modules[m]);
+			result.push(data);
+		}catch(err){
+			//DO something
+		}
+	}
+	return result;
+}
 
-function Builder()
+function writeFile(path){
+	files.writeFile(path, function(err){
+		if(err)console.log(err);
+	});
+}
+
+
+function Response(data, name, action){
+	return {"action":action, "time":new Date(), "result": data[0](data[1]), 
+		   "path": data[1],
+		   "name": name};
+}
+
+function Action(action, data){
+	//return action(data);
+}
+
+
+//Function for store data 
+function Store(){
+
+}
+
+function Builder(name)
 {
+	this.named = name
+	if(this.named == undefined)
+		this.named = "default";
 	this.fast = new FastList();
 }
 
 Builder.prototype = {
 	read: function(path){
-		this.fast.push([readFile, path]);
+		this.fast.push(Response([readFile, path], this.named, 'read'));
 	},
 	mkdir: function(path, err){
 		fast = this.fast;
 		mkdirp(path, function(e){
 			if(e){
-				fast.push([logMessage, message]);
+				fast.push([logMessage, message], this.named, 'create folders');
 			}
 		});
 	},
 	log: function(message){
-		this.fast.push([logMessage, message]);
+		this.fast.push(Response([logMessage, message], this.named, 'log message'));
+	},
+
+	//List of modules
+	modules: function(lmodules){
+		this.fast.push(Response([loadModules, lmodules], this.named, 'load modules'));
+	},
+
+	write: function(path, data){
+		this.fast.push(Action(path, data, this.named));
 	},
 
 	//run all events
 	run: function(){
+		var store = Store();
 		for(var i = 0; i <=this.fast.length;++i){
 			var lst = this.fast.pop();
-			console.log(lst[0](lst[1]));
+			console.log(lst);
 		}
 	}
 }
