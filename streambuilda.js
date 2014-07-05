98d9dbcabd61c7ac63fc94b3f837a855dcfccb57
Sergey Romanov <xxsmotur@gmail.com>
2014-07-05 23:42:36 +0600

    Append user action and write in file

--- streambuilda.js
+++ streambuilda.js
@@ -34,7 +34,7 @@ function GraphQueue(){
 //Чтение файла
 function readFile(name){
 	if(typeof name == 'string'){
-		return files.readFileSync(name, {encoding: 'utf-8'});
+		return readFileHelpful(name);
 	}
 	else
 	{
@@ -43,6 +43,14 @@ function readFile(name){
 			result.push(files.readFileSync(name[n], {encoding: 'utf-8'}));
 		}
 		return result;
+	}
+}
+
+function readFileHelpful(name){
+	try{
+		return files.readFileSync(name, {encoding: 'utf-8'});
+	}catch(e){
+		return 'error to load file ' + name;
 	}
 }
 
@@ -76,8 +84,8 @@ function loadModules(modules){
 	return result;
 }
 
-function writeFile(path){
-	files.writeFile(path, function(err){
+function writeFile(path, data){
+	files.writeFile(path, data, function(err){
 		if(err)console.log(err);
 	});
 }
@@ -86,17 +94,17 @@ function writeFile(path){
 function Response(data, name, action){
 	return {"action":action, "time":new Date(), "result": data[0](data[1]), 
 		   "path": data[1],
-		   "name": name};
+		   "name": name, 'type':'response'};
 }
 
-function Action(action, data){
-	//return action(data);
+function Action(action, path, name){
+	return {'type':'action', "func": action, "path": path, "time":new Date(), 'name':name}
 }
 
 
 //Function for store data 
 function Store(){
-
+	var readStorage = '';
 }
 
 function Builder(name)
@@ -115,10 +123,16 @@ Builder.prototype = {
 		fast = this.fast;
 		mkdirp(path, function(e){
 			if(e){
-				fast.push([logMessage, message], this.named, 'create folders');
+				this.fast.push([logMessage, message], this.named, 'create folders');
 			}
 		});
 	},
+
+	//action with loaded data
+	action: function(func){
+		this.fast.push(Action(func, this.named, 'action'));
+	},
+
 	log: function(message){
 		this.fast.push(Response([logMessage, message], this.named, 'log message'));
 	},
@@ -128,16 +142,26 @@ Builder.prototype = {
 		this.fast.push(Response([loadModules, lmodules], this.named, 'load modules'));
 	},
 
-	write: function(path, data){
-		this.fast.push(Action(path, data, this.named));
+	write: function(path){
+		this.fast.push(Action(writeFile, path, this.named));
 	},
 
 	//run all events
 	run: function(){
-		var store = Store();
+		var store = new Store();
+		var queue = new FastList();
 		for(var i = 0; i <=this.fast.length;++i){
-			var lst = this.fast.pop();
-			console.log(lst);
+			var lst = this.fast.shift();
+			if(lst['type'] == 'response')
+			{
+					if(lst['action'] == 'read'){
+						queue.push(lst['result']);
+					}
+			}
+			if(lst['type'] == 'action')
+			{
+				lst['func'](lst['path'], queue.pop());
+			}
 		}
 	}
 }
