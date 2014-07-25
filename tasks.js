ff362f7f6d6463718d1d83e332aa682b50792583
Sergey Romanov <xxsmotur@gmail.com>
2014-07-25 23:57:22 +0600

    Some of rewrite and append in task system

--- tasks.js
+++ tasks.js
@@ -15,16 +15,15 @@ var TaskGraph = function(){
 	var graph = {};
 	return {
 		set: function(name, func){
-			var append = {'name': name, 'func': func, result:undefined, 'type':'single'};
+			var append = {'name': name, 'func': func, result:undefined, 'type':'single', 
+			'args': undefined};
 			graph[name] = append;
 		},
 		//Also, with arguments with tasks
 		//name is parent
 		setParents: function(array){
-			var arglen = array.length;
-			var nodes = array.slice(2, arglen);
-			graph[array[0]] = {'count': Object.keys(nodes).length, 'type': 'complex', 
-			'nodes': nodes};
+			graph[array['name']] = {'count': Object.keys(array).length, 'type': 'complex', 
+			'nodes': array['tasks'], 'args': undefined, 'argsfrom':undefined, 'func': array['func']};
 		},
 
 		_getNodes: function(nodes){
@@ -60,40 +59,53 @@ var TaskGraph = function(){
 	
 */
 
-function TaskSystem(){
-	this.gr = new TaskGraph();
-}
+var TaskSystem = (function(){
+	var gr = new TaskGraph();
+	return {
+		task: function(name,data){
+			var args = Array.prototype.slice.call(arguments, 0)[0];
+			if(Object.keys(args).length == 2)
+				gr.set(args['name'], args['func']);
+			else{
+				gr.setParents(args)
+			}
+		},
 
-//Need
-TaskSystem.prototype = {
-	task: function(name
-		, data){
-		if(arguments.length == 2)
-			this.gr.set(name, data);
-		else{
-			var array = Array.prototype.slice.call(arguments, 0);
-			this.gr.setParents(array)
+		//Append arguments
+		args: function(name){
+			var arg = Array.prototype.slice.call(arguments, 1)[0];
+			value = arg['value'];
+			if(value != undefined)
+				gr.update(name, 'args', value);
+			var argsfrom = arg['argsfrom'];
+			if(argsfrom != undefined)
+				gr.update(name, 'argsfrom', argsfrom)
+		},
+
+		run: function(){
+			var graph = gr;
+			var complexNodes = graph.getComplexNodes();
+			if(complexNodes.length == 0)
+				throw new EmptyTaskException('This node has no tasks');
+			complexNodes.forEach(function(x){
+				var nodes = graph.get(x)['nodes'];
+				nodes.forEach(function(y){
+					var singlenode = graph.get(y);
+					console.log(singlenode);
+					if(singlenode.result == undefined){
+						//console.log(singlenode.args);
+						var result = singlenode.func(singlenode.args);
+						graph.update(singlenode.name, 'result', result);
+					}
+				});
+
+				//Use result in list of nodes
+				var nodes = graph.get(x)['argsfrom'];
+				var listofresults = nodes.map(function(x){ return graph.get(x)['result']});
+				var result = graph.get(x)['func'].apply(this, listofresults);
+				graph.update(x, 'result', result);
+				
+			})
 		}
-	},
-	run: function(){
-		var graph = this.gr;
-		var complexNodes = graph.getComplexNodes();
-		if(complexNodes.length == 0)
-			throw new EmptyTaskException('This node has no tasks');
-		complexNodes.forEach(function(x){
-			var nodes = graph.get(x)['nodes'];
-			nodes.forEach(function(y){
-				var singlenode = graph.get(y);
-				if(singlenode.result == undefined){
-					var result = singlenode.func(singlenode.arguments);
-					graph.update(singlenode.name, 'result', result);
-				}
-			});
-
-			//Use result in list of nodes
-			console.log(graph.get(x));
-		});
 	}
-}
-
-
+})();
